7317d59bea27ef0cd486ca60cfc58dd0177f8f98
Lucas Americano do Brasil Santos <lucasamericanov8@dinfor.uniceub>
2024-10-07 19:04:52 -0300

    Introdução ao React

---
+++ !REACT/teste/src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+export default function Header() {
+    return (
+        <header>
+            <h1>LOGO</h1>
+            <div>
+                <a href="">Contato</a>
+                <a href="">Projetos</a>
+            </div>
+        </header>
+    )
+    
+}
