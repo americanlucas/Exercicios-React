8cf50e0606e1592a6a3d7e16bd1631d7041afbed
americanlucas <lucasamericanov8@gmail.com>
2024-10-17 12:14:52 -0300

    Criar Página Cadastro

---
+++ !REACT/PaginaCadastro/README.md
@@ -0,0 +1,4 @@
+# TELA DE CADASTRO -REACT
+
+Criada uma tela de cadastro usando react para manipular 
+### HTML E CSS
---
+++ !REACT/PaginaCadastro/src/Componentes/Main/index.jsx
@@ -0,0 +1,14 @@
+import "./style.css"
+
+export default function Faz_o_L() {
+    return (
+        <>
+            <form action="" id="esquerda">
+                <h3>Informações</h3>
+                <input type="text" placeholder="Nome Completo" required />
+                <input type="text" placeholder="Número de Telefone" required />
+                <input type="text" placeholder="Endereço" required />
+            </form>
+        </>
+    )
+}
